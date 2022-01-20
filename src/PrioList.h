302c2b05a4848840cb988c89861af427ad44cee3
Cornelius Marx <marxc9@outlook.de>
2022-01-20 22:06:02 +0100

    removing backward insertion - hopeless

--- src/PrioList.h
+++ src/PrioList.h
@@ -12,31 +12,7 @@ public:
 
 	// insert according to priority
 	typename std::list<T>::iterator insert(const T & element){
-		// we start from the end, because for the specific case of SQEM metric,
-		// when an edge is collapsed the new edges will likely have a very high cost,
-		// therefore shortening the search time for the correct position in the list
 		Compare cmp;
-		/*
-		if(this->empty() || !cmp(element, this->back())){
-			return this->std::list<T>::insert(this->end(), element);
-		}
-		typename std::list<T>::iterator it = this->end();
-		it--;
-
-		
-		while(it != this->end()){
-			// compare both elements
-			if(!cmp(element, *it)){
-				// insert after current
-				it++;
-				return this->std::list<T>::insert(it, element);
-			}
-			it--;
-		}
-
-		// smallest element, put at front
-		return this->std::list<T>::insert(this->begin(), element);
-		*/
 		typename std::list<T>::iterator it = this->begin();
 		while(it != this->end()){
 			// compare both elements
